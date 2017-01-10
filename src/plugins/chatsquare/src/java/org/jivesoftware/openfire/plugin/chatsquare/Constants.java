b5035c6b640c0443feeb521dc43370ef86cb53b3
Ulaş Sancak <ulas.sancak@metus.com>
2017-01-10 20:36:20 +0300

    RestAPI library fix

--- src/plugins/chatsquare/src/java/org/jivesoftware/openfire/plugin/chatsquare/Constants.java
+++ src/plugins/chatsquare/src/java/org/jivesoftware/openfire/plugin/chatsquare/Constants.java
@@ -1,7 +1,7 @@
 package org.jivesoftware.openfire.plugin.chatsquare;
 
 public class Constants {
-	public static final String SECRET_KEY = "A51u7hE3sovPW391";
+	public static final String SECRET_KEY = "Gb1x59JosP6LA0W5";
 	public static final String PARSE_URL = "http://localhost:5050/chatsquare";
 	public static final String PARSE_APP_ID = "me.chatsquare";
 	public static final String PARSE_REST_API_KEY = "o9ybU872O5UZ5070t32A2953r7DFKgcg";
